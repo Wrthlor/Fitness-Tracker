5752d55ca727a9f25d26a5c82c47200129c413bf
wrthlor <nickster179@gmail.com>
2021-03-13 00:15:46 -0600

    Refactored SQL schema and test data, added SQL logic design

--- SQL/data.sql
+++ SQL/data.sql
@@ -1,69 +1,59 @@
--- SAMPLE TEST DATA (WIP) --
+-- Sample "Hard Coded" Data --
+-- Based off PHUL routine --
 
--- Sample person data
-INSERT INTO person (person_name)
-VALUES 
-    ('Jack'),
-    ('Jill');
+-- Default exercise categories 
+INSERT INTO categories (category_name)
+VALUES
+    ('Shoulders'), ('Triceps'), ('Biceps'), 
+    ('Chest'), ('Back'), ('Legs'), 
+    ('Abs'), ('Cardio');
 
--- Sample routine data
-INSERT INTO routines (
-    routine_name,
-    descriptions
-)
-VALUES 
-    ('Pull', 'pull exercises'),
-    ('Push', 'push exercises'),
-    ('Legs', 'leg exercises'),
-    ('Chest', 'chest exercises'),
-    ('Back', 'back exercises'),
-    ('Arms', 'arm exercises');
-
--- Sample exercises data
-INSERT INTO exercises (exercise_name)
-VALUES 
-    ('Deadlift'),
-    ('Bent-Over Row'),
-    ('Pull-Up'),
-    ('Flat Bench Press'),
-    ('Barbell Overhead Press'),
-    ('Parallel-bar Dips'),
-    ('Back Squat'),
-    ('Dumbbell Lunge'),
-    ('Calf Raise'),
-    ('Barbell Bicep Curl');
-
--- Sample personRoutine data
-INSERT INTO personalRoutine (
-    personalRoutine_name,
-    person_id,
-    routine_id
+-- Default exercise list data 
+INSERT INTO exercises (
+    exercise_name,
+    categories_id
 )
 VALUES
-    ('Jack Pull Routine', 1, 1), ('Jack Push Routine', 1, 2), ('Jack Legs Routine', 1, 3),
-    ('Jill Chest Routine', 2, 4), ('Jill Back Routine', 2, 5), ('Jill Arm Routine', 2, 6), ('Jill Legs Routine', 2, 3);
+    ('Arnold Dumbbell Press', 1), ('Behind The Neck Barbell Press', 1), ('Cable Face Pull', 1), ('Front Dumbbell Raise', 1), ('Hammer Strength Shoulder Press', 1), ('Lateral Dumbbell Raise', 1), ('Lateral Machine Raise', 1), ('Log Press', 1), ('One-Arm Standing Dumbbell Press', 1), ('Overhead Press', 1), ('Push Press', 1), ('Rear Delt Dumbbell Raise', 1), ('Seated Dumbbell Press', 1), ('Smith Machine Overhead Press', 1),
+    ('Cable Overhead Triceps Extension', 2), ('Close Grip Barbell Bench Press', 2), ('Dumbbell Overhead Triceps Extension', 2), ('EZ-Bar Skullcrusher', 2), ('Lying Triceps Extension', 2), ('Parallel Bar Triceps Dip', 2), ('Ring Dip', 2), ('Rope Push Down', 2), ('Smith Machine Close Grip Bench Press', 2), ('V-Bar Push Down', 2),
+    ('Barbell Curl', 3), ('Cable Curl', 3), ('Dumbbell Concentration Curl', 3), ('Dumbbell Curl', 3), ('Dumbbell Hammer Curl', 3), ('Dumbbell Preacher Curl', 3), ('EZ-Bar Curl', 3), ('EZ-Bar Preacher Curl', 3), ('Seated Incline Dumbbell Curl', 3), ('Seated Machine Curl', 3),
+    ('Cable Crossover', 4), ('Decline Barbell Bench Press', 4), ('Decline Hammer Strength Chest Press', 4), ('Flat Barbell Bench Press', 4), ('Flat Dumbbell Bench Press', 4), ('Flat Dumbbell Fly', 4), ('Incline Barbell Bench Press', 4), ('Incline Dumbbell Bench Press', 4), ('Incline Dumbbell Fly', 4), ('Incline Hammer Strength Chest Press', 4), ('Seated Machine Fly', 4),
+    ('Barbell Row', 5), ('Barbell Shrug', 5), ('Chin Up', 5), ('Deadlift', 5), ('Dumbbell Row', 5), ('Good Morning', 5), ('Hammer Strength Row', 5), ('Lat Pulldown', 5), ('Machine Shrug', 5), ('Neutral Chin Up', 5), ('Pendlay Row', 5), ('Pull Up', 5), ('Rack Pull', 5), ('Seated Cable Row', 5), ('Straight-Arm Cable Pushdown', 5), ('T-Bar Row', 5),
+    ('Barbell Calf Raise', 6), ('Barbell Front Squat', 6), ('Barbell Glute Bridge', 6), ('Barbell Squat', 6), ('Donkey Calf Raise', 6), ('Glute-Ham Raise', 6), ('Leg Extension Machine', 6), ('Leg Press', 6), ('Lying Leg Curl Machine', 6), ('Romanian Deadlift', 6), ('Seated Calf Raise Machine', 6), ('Seated Leg Curl Machine', 6), ('Standing Calf Raise Machine', 6), ('Stiff-Legged Deadlift', 6), ('Sumo Deadlift', 6),
+    ('Ab-Wheel Rollout', 7), ('Cable Crunch', 7), ('Crunch', 7), ('Crunch Machine', 7), ('Decline Crunch', 7), ('Dragon Flag', 7), ('Hanging Knee Raise', 7), ('Hanging Leg Raise', 7), ('Plank', 7), ('Side Plank', 7),
+    ('Cycling', 8), ('Elliptical Trainer', 8), ('Rowing Machine', 8), ('Running (Outdoor)', 8), ('Running (Treadmill)', 8), ('Stationary Bike', 8), ('Swimming', 8), ('Walking', 8)
+;
 
--- Sample workout data
+-- Sample sets based off PHUL routine
+INSERT INTO metrics (
+    created_at, 
+    wgt, 
+    reps
+)
+VALUES 
+    ('2021-03-01 07:00:00', 165, 3), ('2021-03-01 07:05:00', 165, 3), ('2021-03-01 07:10:00', 165, 3), ('2021-03-01 07:15:00', 165, 3), ('2021-03-01 07:20:00', 165, 3), ('2021-03-01 07:25:00', 140, 5), ('2021-03-01 07:30:00', 140, 5), ('2021-03-01 07:35:00', 140, 5), ('2021-03-01 07:40:00', 140, 5), ('2021-03-01 07:45:00', 105, 4), ('2021-03-01 07:50:00', 105, 3), ('2021-03-01 07:55:00', 105, 3), ('2021-03-01 08:00:00', 105, 2), ('2021-03-01 08:05:00', 140, 5), ('2021-03-01 08:10:00', 140, 5), ('2021-03-01 08:15:00', 140, 5), ('2021-03-01 08:20:00', 140, 5), 
+    ('2021-03-02 07:00:00', 225, 3), ('2021-03-02 07:05:00', 225, 3), ('2021-03-02 07:10:00', 225, 3), ('2021-03-02 07:15:00', 225, 3), ('2021-03-02 07:20:00', 225, 3), ('2021-03-02 07:25:00', 375, 10), ('2021-03-02 07:30:00', 375, 10), ('2021-03-02 07:35:00', 375, 10), ('2021-03-02 07:40:00', 155, 15), ('2021-03-02 07:45:00', 155, 15), ('2021-03-02 07:50:00', 155, 15), 
+    ('2021-03-04 07:00:00', 135, 7), ('2021-03-04 07:05:00', 135, 7), ('2021-03-04 07:10:00', 135, 7), ('2021-03-04 07:15:00', 135, 7), ('2021-03-04 07:20:00', 160, 5), ('2021-03-04 07:25:00', 160, 5), ('2021-03-04 07:30:00', 160, 5), ('2021-03-04 07:35:00', 40, 10), ('2021-03-04 07:40:00', 40, 10), ('2021-03-04 07:45:00', 40, 10), ('2021-03-04 07:50:00', 40, 10), ('2021-03-04 07:55:00', 100, 10), ('2021-03-04 08:00:00', 100, 10), ('2021-03-04 08:05:00', 100, 10), ('2021-03-04 08:10:00', 100, 10), 
+    ('2021-03-05 07:00:00', 220, 3), ('2021-03-05 07:05:00', 220, 3), ('2021-03-05 07:10:00', 220, 3), ('2021-03-05 07:15:00', 220, 3), ('2021-03-05 07:20:00', 220, 3), ('2021-03-05 07:25:00', 140, 7), ('2021-03-05 07:30:00', 140, 7), ('2021-03-05 07:35:00', 140, 7), ('2021-03-05 07:40:00', 140, 7), ('2021-03-05 07:45:00', 170, 12), ('2021-03-05 07:50:00', 170, 12), ('2021-03-05 07:55:00', 170, 12), ('2021-03-05 08:00:00', 170, 12), ('2021-03-05 08:05:00', 70, 10), ('2021-03-05 08:10:00', 70, 10), ('2021-03-05 08:15:00', 70, 10), ('2021-03-05 08:20:00', 70, 10)
+;
+
+-- Sample workouts based off PHUL routine
 INSERT INTO workouts (
-    workouts_name,
-    routine_id,
-    exercise_id
+    exercise_id, 
+    metrics_id, 
+    logs_id
 )
 VALUES 
-    ('Workout 1', 1, 1), ('Workout 1', 1, 2), ('Workout 1', 1, 3),
-    ('Workout 1', 2, 4), ('Workout 1', 2, 5), ('Workout 1', 2, 6),
-    ('Workout 1', 3, 7), ('Workout 1', 3, 8), ('Workout 1', 3, 9),
-    ('Workout 2', 4, 4), ('Workout 2', 4, 5),
-    ('Workout 2', 5, 2), ('Workout 2', 4, 3),
-    ('Workout 2', 6, 10),
-    ('Workout 2', 3, 1), ('Workout 2', 3, 7);
+    (38, 1, 1), (38, 2, 1), (38, 3, 1), (38, 4, 1), (38, 5, 1), (56, 6, 1), (56, 7, 1), (56, 8, 1), (56, 9, 1), (10, 10, 1), (10, 11, 1), (10, 12, 1), (10, 13, 1), (53, 14, 1), (53, 15, 1), (53, 16, 1), (53, 17, 1), 
+    (49, 18, 2), (49, 19, 2), (49, 20, 2), (49, 21, 2), (49, 22, 2), (69, 23, 2), (69, 24, 2), (69, 25, 2), (72, 26, 2), (72, 27, 2), (72, 28, 2),
+    (38, 29, 3), (38, 30, 3), (38, 31, 3), (38, 32, 3), (57, 33, 3), (57, 34, 3), (57, 35, 3), (42, 36, 3), (42, 37, 3), (42, 38, 3), (42, 39, 3), (59, 40, 3), (59, 41, 3), (59, 42, 3), (59, 43, 3),
+    (65, 44, 4), (65, 45, 4), (65, 46, 4), (65, 47, 4), (65, 48, 4), (71, 49, 4), (71, 50, 4), (71, 51, 4), (71, 52, 4), (72, 53, 4), (72, 54, 4), (72, 55, 4), (72, 56, 4), (73, 57, 4), (73, 58, 4), (73, 59, 4), (73, 60, 4)
+;
 
--- Sample schedule data
-INSERT INTO schedule (
-    schedule_name,
-    personalRoutine_id,
-    workout_id
-)
+-- Sample log for PHUL routine (4 days)
+INSERT INTO logs (created_at)
 VALUES
-    ('PPL', 1, 1), ('PPL', 1, 2), ('PPL', 1, 3), ('PPL', 1, 4), ('PPL', 1, 5), ('PPL', 1, 6), ('PPL', 1, 7), ('PPL', 1, 8), ('PPL', 1, 9), 
-    ('Bro split', 2, 10), ('Bro split', 2, 11), ('Bro split', 2, 12), ('Bro split', 2, 13), ('Bro split', 2, 14), ('Bro split', 2, 15), ('Bro split', 2, 16);
+    ('2021-03-01 07:00:00'),   
+    ('2021-03-02 07:00:00'), 
+    ('2021-03-04 07:00:00'), 
+    ('2021-03-05 07:00:00');
--- SQL/setup.sql
+++ SQL/setup.sql
@@ -2,52 +2,50 @@
 CREATE DATABASE IF NOT EXISTS fitnessTracker;
 USE fitnessTracker;
 
--- Create tables to imiate exercise tracker [if tables don't exist]
--- Table for individual person
-CREATE TABLE IF NOT EXISTS person (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    person_name VARCHAR(100) UNIQUE NOT NULL,
+-- Each row is for one unique exercise category
+-- Based off primary muscle mover 
+-- Ex. Back for "pull up"
+CREATE TABLE IF NOT EXISTS categories (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    category_name VARCHAR(255) UNIQUE NOT NULL
+);
+
+-- Each row is one unique exercise
+CREATE TABLE IF NOT EXISTS exercises (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    exercise_name VARCHAR(100) UNIQUE NOT NULL,
+    categories_id INT NOT NULL,
+    FOREIGN KEY (categories_id) REFERENCES categories(id)
+);
+
+-- Each metrics row is one set
+-- Ex. 135 lbs at 3 reps
+CREATE TABLE IF NOT EXISTS metrics (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    wgt DECIMAL(10,1) DEFAULT 0,
+    reps INT NOT NULL,    
     created_at TIMESTAMP DEFAULT NOW()
 );
 
--- Table for routines
-CREATE TABLE IF NOT EXISTS routines (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    routine_name VARCHAR(100) NOT NULL,
-    descriptions VARCHAR(255)
-);
-
--- Table for individual exercises
-CREATE TABLE IF NOT EXISTS exercises (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    exercise_name VARCHAR(100) UNIQUE NOT NULL
-);
-
--- Table for a individual person's routine
-CREATE TABLE IF NOT EXISTS personalRoutine (
-    personalRoutine_name VARCHAR(255) NOT NULL,     -- Ex. "Jack's routine"
-    person_id INT NOT NULL,                         -- Keeps associated with name
-    routine_id INT NOT NULL,
-    FOREIGN KEY (person_id) REFERENCES person(id),
-    FOREIGN KEY (routine_id) REFERENCES routines(id)
-);
-
--- Table for workout plans (comprised of exercises)
+-- Individual workout contains single exercise done and corresponding reps/set
 CREATE TABLE IF NOT EXISTS workouts (
-    workouts_name VARCHAR(255) NOT NULL,             -- Ex. "Pull exercises" or "Arm workout"
-    workouts_id INT AUTO_INCREMENT PRIMARY KEY,      -- Keeps associated with name
-    routine_id INT NOT NULL,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     exercise_id INT NOT NULL,
-    FOREIGN KEY (routine_id) REFERENCES routines(id),
-    FOREIGN KEY (exercise_id) REFERENCES exercises(id)
+    metrics_id INT NOT NULL,
+    logs_id INT NOT NULL,
+    FOREIGN KEY (exercise_id) REFERENCES exercises(id),
+    FOREIGN KEY (metrics_id) REFERENCES metrics(id)
 );
 
--- Table for schedules (compilation of workouts per person)
-CREATE TABLE IF NOT EXISTS schedule (
-    schedule_name VARCHAR(100) NOT NULL,
-    schedule_id INT AUTO_INCREMENT PRIMARY KEY,
-    personalRoutine_id INT NOT NULL,
-    workout_id INT NOT NULL,
-    FOREIGN KEY (personalRoutine_id) REFERENCES personalRoutine(person_id),
-    FOREIGN KEY (workout_id) REFERENCES workouts(workouts_id)
+-- Log to associate user (in future) and workout per day
+CREATE TABLE IF NOT EXISTS logs (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    created_at TIMESTAMP DEFAULT NOW()
 );
+
+-- -- Users table (for future)
+-- CREATE TABLE IF NOT EXISTS users (
+--     id INT PRIMARY KEY AUTO_INCREMENT,
+--     created_at TIMESTAMP DEFAULT NOW(),
+--     logs_id INT NOT NULL
+-- )
