d131aa057bc4bd11a58d0d9f9afc07b59ebd14b9
wrthlor <nickster179@gmail.com>
2021-03-18 01:24:10 -0500

    First iteration of HTTP requests, refactored SQL schema - ON CASCADE DELETE for metrics and workouts

--- SQL/setup.sql
+++ SQL/setup.sql
@@ -6,35 +6,26 @@ USE fitnessTracker;
 -- Based off primary muscle mover 
 -- Ex. Back for "pull up"
 CREATE TABLE IF NOT EXISTS categories (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     category_name VARCHAR(255) UNIQUE NOT NULL
 );
 
 -- Each row is one unique exercise
 CREATE TABLE IF NOT EXISTS exercises (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     exercise_name VARCHAR(100) UNIQUE NOT NULL,
     categories_id INT NOT NULL,
-    FOREIGN KEY (categories_id) REFERENCES categories(id)
+    FOREIGN KEY (categories_id) 
+        REFERENCES categories(id)
 );
 
 -- Each metrics row is one set
 -- Ex. 135 lbs at 3 reps
 CREATE TABLE IF NOT EXISTS metrics (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT  AUTO_INCREMENT PRIMARY KEY,
     wgt DECIMAL(10,1) DEFAULT 0,
     reps INT NOT NULL,    
     created_at TIMESTAMP DEFAULT NOW()
-);
-
--- Individual workout contains single exercise done and corresponding reps/set
-CREATE TABLE IF NOT EXISTS workouts (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    exercise_id INT NOT NULL,
-    metrics_id INT NOT NULL,
-    logs_id INT NOT NULL,
-    FOREIGN KEY (exercise_id) REFERENCES exercises(id),
-    FOREIGN KEY (metrics_id) REFERENCES metrics(id)
 );
 
 -- Log to represent per day
@@ -42,6 +33,19 @@ CREATE TABLE IF NOT EXISTS workouts (
 CREATE TABLE IF NOT EXISTS logs (
     id INT PRIMARY KEY AUTO_INCREMENT,
     created_at TIMESTAMP DEFAULT NOW()
+);
+
+-- Individual workout contains single exercise done and corresponding reps/set
+CREATE TABLE IF NOT EXISTS workouts (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    exercise_id INT NOT NULL,
+    metrics_id INT NOT NULL,
+    logs_id INT NOT NULL,
+    FOREIGN KEY (exercise_id) 
+        REFERENCES exercises(id),
+    FOREIGN KEY (metrics_id) 
+        REFERENCES metrics(id) 
+        ON DELETE CASCADE
 );
 
 -- -- Users table (for future)
